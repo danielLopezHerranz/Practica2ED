a9e12cb494d2ed7972dc1570dd98453d21333d0e
danielLopezHerranz <109759279+danielLopezHerranz@users.noreply.github.com>
2023-02-27 18:00:04 +0100

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,23 @@
+/**
+ * Clase Principal, desde la que se llama a la clase Pruebas
+ *
+ * Esta clase se entrega completamente desarrollada, no se tiene que modificar
+ *
+ * @author Pilar Martinez Garcia
+ * @version 1.0
+ */
+public class Principal {//Esta clase se daría completa, tal y como está
+    public static void main (String [] args) {
+        
+        Pruebas prueba = new Pruebas();
+        prueba.probarOpciones ();
+        Pila pilaPruebas = new Pila();
+        pilaPruebas.apilar(1);
+        pilaPruebas.apilar(2);
+        pilaPruebas.apilar(3);
+        pilaPruebas.apilar(4);
+        pilaPruebas.apilar(5);
+        System.out.print("1.MostrarInverso.\tElija una opción: ");
+        int elección=
+    }
+}
